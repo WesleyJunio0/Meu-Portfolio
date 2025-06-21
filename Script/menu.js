2406fcc93689799b5350e5ad22f91219daa093f5
Sleyd1 <211575227+WesleyJunio0@users.noreply.github.com>
2025-06-21 18:08:01 -0300

    definindo layout do menu em diferentes telas

--- Script/menu.js
+++ Script/menu.js
@@ -24,3 +24,37 @@ function navegar(valor) {
     menu.style.display = 'none';
   }
 }
+
+
+/* responsividade do menu */
+
+function telasMaiores(){
+    const menu = document.querySelector('.display');
+    const span = document.querySelector('#span1');
+    const icon = document.querySelectorAll('.icons-dentro-do-menu ');
+    
+  if (window.innerWidth >= 768){
+       menu.style.display = 'block';
+       span.style.display = 'none';
+       icon[0].style.display = 'none'
+       icon[1].style.display = 'none'
+       icon[2].style.display = 'none'
+       icon[3].style.display = 'none'
+  }
+  else {
+      menu.style.display = 'none';
+      span.style.display = 'block';
+       icon[0].style.display = 'block'
+       icon[1].style.display = 'block'
+       icon[2].style.display = 'block'
+       icon[3].style.display = 'block'
+  }
+
+}
+
+
+window.addEventListener('resize', telasMaiores);
+
+window.addEventListener('load', telasMaiores);
+
+ 
